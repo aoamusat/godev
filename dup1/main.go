afc06c601b744abcb7847753f1b82e2f911d6e4e
Akeem <aoamusat@gmail.com>
2025-11-24 10:26:11 +0100

    gopl.io/ch1/dup1

---
+++ dup1/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counter := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+
+	for input.Scan() {
+		counter[input.Text()]++
+	}
+
+	for line, n := range counter {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
