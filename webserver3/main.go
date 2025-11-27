c8d9173f55468275bd820945f2b62d1734770a87
Akeem <aoamusat@gmail.com>
2025-11-27 12:39:08 +0100

    Webserver 3

---
+++ webserver3/main.go
@@ -0,0 +1,28 @@
+// Server2 is a minimal "echo" and counter server.
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", requestHandler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func requestHandler(w http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(w, "%s %s %s\n", request.Method, request.URL, request.Proto)
+	for k, v := range request.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", request.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", request.RemoteAddr)
+	if err := request.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range request.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
+}
