5eaf215326ac6a13834ed8eddd837dadd813613e
Akeem Amusat <aoamusat@gmail.com>
2025-12-16 04:59:19 +0100

    json

--- main.go
+++ main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"a4m.dev/godev/chapter4/treesort"
+	"a4m.dev/godev/chapter4/json"
 )
 
 func main() {
-	tree := treesort.NewTree("Lagos")
-	treesort.Add(tree, "Abuja")
-	treesort.Add(tree, "Kano")
-	treesort.Add(tree, "Enugu")
-	treesort.Add(tree, "Ibadan")
-	treesort.Add(tree, "Calabar")
-	treesort.PrintTree(tree, 0)
+	json.PrintMovies()
 }
